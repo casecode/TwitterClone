238686638b9bfcf48e65e3e20183c41f1f1b56d2
Casey White <casey.r.white@gmail.com>
2014-10-06 13:34:34 -0700

    add tweets model; read json file of tweets and display in table view

---
+++ TwitterClone/HomeTimelineViewController.swift
@@ -0,0 +1,45 @@
+//
+//  HomeTimelineViewController.swift
+//  TwitterClone
+//
+//  Created by Casey R White on 10/6/14.
+//  Copyright (c) 2014 casecode. All rights reserved.
+//
+
+import UIKit
+
+class HomeTimelineViewController: UITableViewController {
+    
+    var tweets : [Tweet]?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let path = NSBundle.mainBundle().pathForResource("tweets", ofType: "json") {
+            var error : NSError?
+            let jsonData = NSData(contentsOfFile: path)
+            self.tweets = Tweet.parseJSONDataIntoTweets(jsonData!)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let tweets = self.tweets {
+            return tweets.count
+        } else {
+            return 0
+        }
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("TWEET_CELL") as UITableViewCell
+        let tweet = self.tweets?[indexPath.row]
+        cell.textLabel?.text = tweet?.text
+        
+        return cell
+    }
+
+}
---
+++ TwitterClone/Tweet.swift
@@ -0,0 +1,38 @@
+//
+//  Tweet.swift
+//  TwitterClone
+//
+//  Created by Casey R White on 10/6/14.
+//  Copyright (c) 2014 casecode. All rights reserved.
+//
+
+import Foundation
+
+class Tweet {
+    
+    var text : String
+    
+    init(tweetDictionary : NSDictionary) {
+        self.text = tweetDictionary["text"] as String
+    }
+    
+    class func parseJSONDataIntoTweets(rawJSONData : NSData) -> [Tweet]? {
+        var error : NSError?
+        if let JSONArray = NSJSONSerialization.JSONObjectWithData(rawJSONData, options: nil, error: &error) as? NSArray {
+            
+            var tweets = [Tweet]()
+            
+            for JSONDictionary in JSONArray {
+                if let tweetDictionary = JSONDictionary as? NSDictionary {
+                    var newTweet = Tweet(tweetDictionary: tweetDictionary)
+                    tweets.append(newTweet)
+                }
+            }
+            
+            return tweets
+        }
+        // else
+        return nil
+    }
+    
+}
